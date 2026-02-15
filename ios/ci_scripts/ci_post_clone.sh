610a2434b6b889af2422f6ccca847d64233a1c2b
Lesetja <kmalapane155@gmail.com>
2026-02-15 15:19:53 +0200

    Fix Xcode Cloud working directory path

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -3,13 +3,14 @@
 # Fail this script if any command fails.
 set -e
 
-cd $CI_WORKSPACE
+# THE FIX: This variable guarantees we are at the very root of your Flutter project
+cd "$CI_PRIMARY_REPOSITORY_PATH"
 
-# 1. Safely install CocoaPods (Adding || true prevents crashing if it's already installed)
+# 1. Safely install CocoaPods
 export HOMEBREW_NO_AUTO_UPDATE=1
 brew install cocoapods || true
 
-# 2. Safely clone Flutter (Only clone if the directory doesn't already exist)
+# 2. Safely clone Flutter
 if [ ! -d "$HOME/flutter" ]; then
   git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
 fi
@@ -18,9 +19,9 @@ export PATH="$PATH:$HOME/flutter/bin"
 # 3. Install Flutter artifacts
 flutter precache --ios
 
-# 4. Get Flutter packages
+# 4. Get Flutter packages (This will now correctly find pubspec.yaml!)
 flutter pub get
 
-# 5. Install iOS Pods (Added --repo-update to ensure Firebase links correctly)
+# 5. Install iOS Pods
 cd ios
 pod install --repo-update
